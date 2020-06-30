c64ae40716fe07766f21b713087d4fd8fefd57d7
blackowe <blackowe@oregonstate.edu>
2020-06-29 19:51:45 -0500

    First Commit attempt -Project_2b

---
+++ temp_convert.py
@@ -0,0 +1,3 @@
+usertemp = float(input("Please enter a Celsius temperature.\n"))    # user input stored in var. as int.
+tempconvert = (9/5*usertemp + 32)   # Celsius to F equation
+print("The equivalent Fahrenheit temperature is:\n", tempconvert)    # prints string
